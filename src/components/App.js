17a4c3795ce363a4593a03dd5340a7aae2fd0290
Ben Jackson <benjacksonSE@gmail.com>
2021-11-03 11:41:28 -0400

    All basic deliverables finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import HogHolder from "./HogHolder";
 import HogFilter from "./HogFilter";
+import HogSorter from "./HogSorter"
 import Nav from "./Nav";
 import hogs from "../porkers_data";
 
 
 function App() {
 	const [filterBy, setFilterBy] = useState("All");
+	const [sortBy, setSortBy] = useState("name");
 
 	//All filter Handlers
 	function handleFilterChange(e) {
@@ -20,11 +22,33 @@ function App() {
 		return hog.greased.toString() === filterBy;
 	})
 
+
+	//All sorting Handlers
+	function handleSortChange(e) {
+		setSortBy(e.target.value)
+	}
+
+	const hogsToDesplay = filteredHogs.sort((a, b) => {
+		if(sortBy === "name") {
+			let x = a.name.toLowerCase();
+      		let y = b.name.toLowerCase();
+      		if (x < y) {
+        		return -50;
+      		}
+      		if (x > y) {
+        		return 50;
+      		}
+      		return 0;
+		}
+		return a.weight - b.weight
+	});
+
 	return (
     <div className="App">
       <Nav />
       <HogFilter handleFilterChange={handleFilterChange} />
-      <HogHolder hogs={filteredHogs} />
+	  <HogSorter handleSortChange={handleSortChange} />
+      <HogHolder hogs={hogsToDesplay} />
     </div>
   );
 }
